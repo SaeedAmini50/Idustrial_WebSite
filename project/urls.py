ce8fad61eb39105aae111b5b20f7a962ab01f7cd
SaeedAmini <saeedamini506750@gmail.com>
2025-02-26 01:18:22 +0330

    connect link with django and change wrighting left to right

--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from project.views import home
+from project.views import home,about,blog,portfolio,service,single,team,contact
 
-
+app_name = 'project'
 urlpatterns = [
     
     path('', home , name='home'),
+    path('about/', about , name='about'),
+    path('blog/', blog , name='blog'),
+    path('portfolio/', portfolio , name='portfolio'),
+    path('service/', service , name='service'),
+    path('single/', single , name='single'),
+    path('team/', team , name='team'),
+    path('contact/', contact , name='contact'),
 ]
--- project/views.py
+++ project/views.py
@@ -3,3 +3,24 @@ from django.http import HttpResponse
 
 def home (requset):
     return render(requset , 'index.html')
+
+def about (requset):
+    return render(requset , 'about.html')
+
+def blog (requset):
+    return render(requset , 'blog.html')
+
+def portfolio (requset):
+    return render(requset , 'portfolio.html')
+
+def service (requset):
+    return render(requset , 'service.html')
+
+def single (requset):
+    return render(requset , 'single.html')
+
+def team (requset):
+    return render(requset , 'team.html')
+
+def contact (requset):
+    return render(requset , 'contact.html')
